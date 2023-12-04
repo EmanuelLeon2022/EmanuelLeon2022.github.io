f93c0e4249cb687c9dbfe36de19513aba9d57524
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-04 15:31:05 -0500

    drops issue fixed

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,7 +6,6 @@ function Display(props) {
             <h1>Category: {props.currentName.data.category}</h1>
             <h1>Description:</h1>
             <p>{props.currentName.data.description}</p>
-            <h1>Drops: {props.currentName.data.drops[0]}</h1>
             <div className="handsome">
                 <h1>Display:</h1>
                 <img src={props.currentName.data.image}/>
