8f1706959408e0dbcf3e9477f80490a9a38bd411
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-04 15:56:57 -0500

    i removed my actual contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div className='feet'>
-        <a href='tel: +1(702)934-2205'><h3>Phone</h3></a>
-        <a href='mailto:emanuell-v2022@outlook.com'><h3>Email</h3></a>
+        <a href='tel: 0000000000'><h3>Phone</h3></a>
+        <a href='mailto:gmail@chucknorris.com'><h3>Email</h3></a>
         <a href='http://www.linkedin.com/in/emanuel-leon-vargas-751720296'><h3>LinkedIn</h3></a>
         <a href='https://github.com/EmanuelLeon2022'><h3>GitHub</h3></a>
     </div>
