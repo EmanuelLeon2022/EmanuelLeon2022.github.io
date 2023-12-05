e95ddadd2583bf632315c2bd170dc013810a3805
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-05 10:10:13 -0500

    responsive site features added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,14 +11,12 @@ function Main() {
         <div className='pain'>
         <h1>Emanuel Leon-Vargas</h1>
         <p>A rising software engineer with extensive training in Javascript(ES6), HTML5 & CSS3, React, and SQL. Able to utilize PostgreSQL, Git, Github, Node.js, set up firewalls, and databases. Experience in problem solving with a background in construction, and some knowledge in mechanical engineering. Solution drive candidate who is seeking to join a software engineering team within an organization.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ullam quae reiciendis illo pariatur maxime ex velit officiis, laboriosam aperiam! Consectetur, voluptate aliquid. Aliquam est quis eveniet ipsam cumque ipsa.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ullam quae reiciendis illo pariatur maxime ex velit officiis, laboriosam aperiam! Consectetur, voluptate aliquid. Aliquam est quis eveniet ipsam cumque ipsa.</p>
         </div>
         <div className='nice'>
-          <img className='images' src="https://media.tenor.com/M46uN9EhkAwAAAAj/fortnite-pixel.gif" alt="" />
-          <img className='images' src="https://64.media.tumblr.com/bdcd75a1aab889887222de90a1ed8246/tumblr_inline_ppm6bn5RHM1rtx1y2_1280.gif" alt="" />
-          <img className='images' src="https://media2.giphy.com/media/kIqoOwOEurUpArjFkn/200w.gif" alt="" />
-          <img className='images' src="https://i.imgur.com/5Io0AoD.gif" alt="" />
+          <img className='image1' src="https://content.instructables.com/F2Z/MOTA/KVAYJU67/F2ZMOTAKVAYJU67.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=4a751bf5dea52de1daa84950b42fa270" alt="Small Gaming console on box" />
+          <img className='image2' src="https://content.instructables.com/FG5/FRPQ/KVAYJU68/FG5FRPQKVAYJU68.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=c8d758db9beea5dedc197c2daf2a3f98" alt="" />
+          <img className='image3' src="https://media2.giphy.com/media/kIqoOwOEurUpArjFkn/200w.gif" alt="" />
+          <img className='image4' src="" alt="" />
         </div>
         <div className='feeter'>
         <Footer/>
