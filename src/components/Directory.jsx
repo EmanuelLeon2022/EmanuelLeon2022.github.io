a780f41d3c7945c5d2d0f195945ffad82dc0676f
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-05 10:37:45 -0500

    new feauture added.

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../App.css'
 import { Link } from 'react-router-dom'
 
 function Directory() {
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,14 +9,18 @@ function Main() {
         <Directory/>
         </div>
         <div className='pain'>
-        <h1>Emanuel Leon-Vargas</h1>
-        <p>A rising software engineer with extensive training in Javascript(ES6), HTML5 & CSS3, React, and SQL. Able to utilize PostgreSQL, Git, Github, Node.js, set up firewalls, and databases. Experience in problem solving with a background in construction, and some knowledge in mechanical engineering. Solution drive candidate who is seeking to join a software engineering team within an organization.</p>
+        <h1 className='manny'>Emanuel Leon-Vargas</h1>
+        <p>Hello visitors and welcome to my website. As you can see there are still a lot of things missing here.</p>
+        <p>It is still a work in progress, but feel free to browse around. On this page is where you will find most of my most recent projects</p>
+        <p>As you can see because some of these words are breaking apart, but don't worry...</p>
+        <p>𐌃ዐ𐌍'𐌕 Ꮤዐ𐌐ዪ𐌙 𐌉ፕ'ነ ል ቻ𐌄ል𐌵ፕ𐌵ዪ𐌄, ክꝊፕ ል ፪𐌵ፏ</p>
+        <p>I mean, D̷͇̹̦̲̭̙͒̅ͣ̿ỡ̢̡̭̗̘̜̺̹͖̪͈̿̈́̌ͫ͗̾ͩ̓͆͆̀̊ͪ̊́̎̚̕͜͡n̴̢̨̹̜͈̱͚̥̖ͨͭͭ̃ͪ̔ͪ͂ͪ̚͘'̳̆t̵̨͇̙͓͓̝͖ͨ̀ͦ͑ w̷̡̡̺̲͇̜̞͍̣̲̳̼͇̥̻ͣ̽͗̒̽͗̒͑ͪ̄̋̃̑͛ͥ͜͠o̶̞͔̹̪̻̪ͥ̀͛͐̾r̴̗͉̔ͬ̽ͧ͗̿͒ͯͥ̂_̴̡̫̰̭̠̽̃͐́͂ͅr͔ͣ̄̊ͧ͛͌y̶̢̝̻ͩ̽̇͊͊̕͜͜͢ i̢̨̗ͪ͆͡͞t͎̞̳̲̰̖̼͖̭̫̩̫͙̻̦̫̣͈̘͆̿ͪ̿̿ͩ̃̑ͧͭ́̎͊͂̀́͘'̶̡̢̪̦̩̙͓̠̲̜͉͔̣̮̀̽ͭ̅̀͜͜͠s̶̴̙̩͚̯̼͕͔̩ͬ͛͑ͭͪ̔͜͠ a̶̸̛̛̯̫̭ͪͭ̃ͬ F͎̆̉͋ͨ̎͝ȩ̵̸̡̟̤͙̞̜̠̰̦̬͍̻̔̇̊ͬͤͯ̏̇́̍͛ͩ͛͌̚ą̵̸̫̙̘͕̝̫̦͔̜ͥ͋̈̃͟u̵̷̞̤̲͇̼̭̖̭̹ͬ͛ͩ̒̿̉ͦͯ͟tu̧̨̧͓͇͍̖̗͈̥̬̜̗̺ͧ̅̑̒̀̈́ͤͦ͗ͩ̿̀͆̕͘͟͡r̴̝͚̜̤̬̳͗̓͋̀̃ͧ̓̋̏̏ͨ́͂ͦͥͮ̍e̷̶̸̛̱͎̟̞̹͖̓̑ͣͪͥͩͩͥ̌ͤ̀ͤ̋͟,̭̣ n̵̨̧̬͎͔͙͙ͭ̅ͨ͂ͬͪ͂̀̾̑ͤ̃̂̽̚̚͢͡ͅo̧̭͎ͯ̓ͬ͝_̷̲̳̼̤̺͓͈̄ͬ͒̒̋̓̐͢͡t̴̵̶̸̶̨̲̦͓̩͔̩̪̝̫͎̜̭ͬ͆ͦ̒͌̌́ͭͤ̈̉͘͢͢ͅ ä̢̧̪̤̪͚̬̰̬͓͒̇ͪ̀̆̔̑͛̄ͭ̓̃̓͜͠͝ͅ_͚͓̣̼͍͙̺̟̥̟̔ͬ͆͠ B̶̴̧̜̣̪͈̱̫͇̦̲͚̪͈̳̫̬͈̓́̀͒ͧ͂̅͋̑̀̀͑̀̐͘͜͢͟͠u̷̶̢̨̬͙͙̫̠̜̙̯ͨ́ͩͯ͗ͩ̎ͬ̓̉͐ͦ͝g̸̶̨̡̝͚͉ͭ̇ͥ̆͌ͥ̈́͊̑͟͜͢</p>
+        <p>ዐⶴ ቻዐዪፏቹፕ ጎፕ</p>
+        <p></p>
         </div>
         <div className='nice'>
           <img className='image1' src="https://content.instructables.com/F2Z/MOTA/KVAYJU67/F2ZMOTAKVAYJU67.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=4a751bf5dea52de1daa84950b42fa270" alt="Small Gaming console on box" />
-          <img className='image2' src="https://content.instructables.com/FG5/FRPQ/KVAYJU68/FG5FRPQKVAYJU68.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=c8d758db9beea5dedc197c2daf2a3f98" alt="" />
-          <img className='image3' src="https://media2.giphy.com/media/kIqoOwOEurUpArjFkn/200w.gif" alt="" />
-          <img className='image4' src="" alt="" />
+          <img className='image2' src="https://content.instructables.com/FG5/FRPQ/KVAYJU68/FG5FRPQKVAYJU68.jpg?auto=webp&frame=1&width=1024&height=1024&fit=bounds&md=c8d758db9beea5dedc197c2daf2a3f98" alt="Sonic 2 running on the Console" />
         </div>
         <div className='feeter'>
         <Footer/>
