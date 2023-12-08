a614f6db9a9c5dd77efbe395ba35001485d3c4b1
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-08 14:19:15 -0500

    css fixed on About Me page.

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -12,13 +12,10 @@ function Me() {
         <h1>&nbsp;</h1>
         <h1>&nbsp;</h1>
         <h1 className='nam'>Emanuel Leon-Vargas</h1>
-        <div className='drop'>
-        <div className='side'>
-          <img src="" alt="" />
-        </div>
         <div className='alpha'>
-          
+          <p>I am a rising software engineer with extensive training in Javascript(ES6), HTML5 & CSS3, React, and SQL. Able to utilize PostgreSQL, Git, Github, Node.js, set up firewalls, and databases. Experience in problem solving with a background in construction, and some knowledge in mechanical engineering. Solution drive candidate who is seeking to join a software engineering team within an organization. </p>
         </div>
+        <div className='drop'>
         <div>
             <p>* Leones Construction - Construction Worker  - Pittsburgh, PA 06/2023 - 08/2023</p>
             <p> Drywall Installation - requires measurement of the workspace, framing 2x4 beams, and drilling the drywall onto the frame to keep it locked in place. Once that is complete, the wall needs to be taped (a process in which you use joint compound to seal crevices in the wall), then painted with a coat of primer.</p>
@@ -29,6 +26,9 @@ function Me() {
             <p>* Cornerstone Christian Preparatory Academy- Custodial - Pittsburgh, PA 05/2023 - 06/2023</p>
             <p> School maintenance- the job was restocking the school bathrooms, restocking classroom cleaning supplies, and cleaning up cafeterias. Cataloging items - required me to identify what items were in stock such as toilet paper, paper towels, soap, rags and record what items were needed.</p>
         </div>
+        </div>
+        <div>
+          
         </div>
         <div className="gnd">
           <Carol/>
