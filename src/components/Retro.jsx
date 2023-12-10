cfa520ad24c802671ee7ecd3b1bba2e91a5832e0
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-10 17:43:42 -0500

    Sonic Spritesheet and animations updated

--- src/components/Retro.jsx
+++ src/components/Retro.jsx
@@ -14,15 +14,12 @@ function Retro() {
   }
   const run = () =>{
     setCurrentSprite(Sprites[1])
-    // console.log(count)
   }
   const push = () =>{
     setCurrentSprite(Sprites[2])
-    // console.log(count)
   }
   const taunt = () =>{
     setCurrentSprite(Sprites[3])
-    // console.log(count)
   }
   class person{
     constructor(name){
@@ -32,37 +29,69 @@ function Retro() {
     move(){
         this.speed ++
     }
-    slow(){
-        this.speed --
-    }
 }
 
 const Sonic = new person('Sonic')
 
+//Sonic's movement key
 window.addEventListener('keydown', e =>{
     if(e.keyCode === 39){
         Sonic.move();
-        run();
-    }
-})
-window.addEventListener('keydown', e =>{
-    if(e.keyCode === 37){
-        Sonic.slow();
+        console.log(Sonic.speed);
         push();
     }
 })
+//This registers keyup as the idle animation
+window.addEventListener('keyup', e =>{
+  if(e.keyCode === 39){
+      idle();
+  }
+})
+
 window.addEventListener('keydown', e =>{
-    if(e.keyCode === 40){
+    if(e.keyCode === 38){
         taunt();
     }
 })
-console.log(Sonic.speed)
+window.addEventListener('keyup', e =>{
+  if(e.keyCode === 38){
+      idle();
+  }
+})
   return (
     <div className='grade'>
-        <Directory/>
+      <Directory />
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <h1 style={{fontFamily: 'face'}}>Emanuel's Cooler Resume</h1>
+      <div className='steam' style={{ position: 'relative', transform: `translateX(${Sonic.speed})` }}>
+        <img src={currentSprite} alt="Sonic the Hedgehog" style={{ height: '120px'}} />
+        <div className='prof'>
+          <h1 style={{fontFamily: 'face', color: 'white'}}>Professional Summary</h1>
+          <p> A rising software engineer with extensive training in Javascript(ES6), HTML5 & CSS3, React, and SQL. Able to utilize PostgreSQL, Git, Github, Node.js, set up firewalls, and databases. Experience in problem solving with a background in construction, and some knowledge in mechanical engineering. Solution drive candidate who is seeking to join a software engineering team within an organization.</p>
+        </div>
+        </div>
+        <h2>&nbsp;</h2>
+        <div className='hyrule'>
+          <img src="" alt="" />
+        <div className='exp'>
+          <h1 style={{fontFamily: 'face', color: 'white'}}>Professional Experience</h1>
+          <p>&nbsp;</p>
+          <div>
+          <h3 style={{fontFamily: 'sans-serif'}}>Leones Construction - Construction Worker  - Pittsburgh, PA 06/2023 - 08/2023</h3>
+          <p>&nbsp;</p>
+          <p style={{fontFamily: 'cursive'}}> Drywall Installation - requires measurement of the workspace, framing 2x4 beams, and drilling the drywall onto the frame to keep it locked in place. Once that is complete, the wall needs to be taped (a process in which you use joint compound to seal crevices in the wall), then painted with a coat of primer.</p>
+          <p>&nbsp;</p>
+          <p style={{fontFamily: 'cursive'}}> Decking Installation - requires measurement of where the platform will be placed, placing multiple 2x8 boards in a row form and jointing them with solid brackets and decking screws, and lastly pacing out the decking floor boards in a way where they are properly spaced between to walk on.</p>
+        </div>
         <p>&nbsp;</p>
-        <p>&nbsp;</p>
-        <img src={currentSprite} alt="Sonic the Hedgehog" style={{height:'100px', transform: `translateX${Sonic.speed}`}}/>
+        <div>
+          <h3 style={{fontFamily: 'sans-serif'}}> Cornerstone Christian Preparatory Academy- Custodial - Pittsburgh, PA 05/2023 - 06/2023</h3>
+          <p>&nbsp;</p>
+          <p style={{fontFamily: 'cursive'}}> School maintenance- the job was restocking the school bathrooms, restocking classroom cleaning supplies, and cleaning up cafeterias. Cataloging items - required me to identify what items were in stock such as toilet paper, paper towels, soap, rags and record what items were needed.</p>
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/models/Sprites.jsx
+++ src/components/models/Sprites.jsx
@@ -1,12 +1,17 @@
 const Sprites = [
-    //Idle animation
-    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6f634735-dd79-4cfb-9f9f-7ed6b339cbfd/dbeiodj-f8d87aa3-c0ab-4ed8-b312-e0831a6f26cb.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZmNjM0NzM1LWRkNzktNGNmYi05ZjlmLTdlZDZiMzM5Y2JmZFwvZGJlaW9kai1mOGQ4N2FhMy1jMGFiLTRlZDgtYjMxMi1lMDgzMWE2ZjI2Y2IuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.JhOvXdbkq64cZhG5X6Dnuir3gXKDSOJ4P7ge8YKq03s",
-    //Moving animation
+    //Sonic Idle animation
+    //"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6f634735-dd79-4cfb-9f9f-7ed6b339cbfd/dbeiodj-f8d87aa3-c0ab-4ed8-b312-e0831a6f26cb.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzZmNjM0NzM1LWRkNzktNGNmYi05ZjlmLTdlZDZiMzM5Y2JmZFwvZGJlaW9kai1mOGQ4N2FhMy1jMGFiLTRlZDgtYjMxMi1lMDgzMWE2ZjI2Y2IuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.JhOvXdbkq64cZhG5X6Dnuir3gXKDSOJ4P7ge8YKq03s",
+    "https://i.redd.it/do7xw42l4yb71.gif",
+    //Sonic Moving animation
     "https://i.gifer.com/origin/aa/aa7d3af3e7c2ac4c8ec0df7f6dddcff1_w200.gif",
-    //Pushing animation
+    //Sonic Pushing animation
     "https://media.tenor.com/w2MnXF-FiPwAAAAi/sonic-pushing-retro-old-sth.gif",
-    //Taunt animation
-    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3db1c2c-2b90-4e60-8e17-b393d0516cc5/dbvn5to-1e78367f-627f-4b08-b25b-6ff79d8e7246.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzZGIxYzJjLTJiOTAtNGU2MC04ZTE3LWIzOTNkMDUxNmNjNVwvZGJ2bjV0by0xZTc4MzY3Zi02MjdmLTRiMDgtYjI1Yi02ZmY3OWQ4ZTcyNDYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dpZ_KpgpC3iUJ6JUO3XjRUBeSGqbeblU1xsY2LO3GT4"
+    //Sonic Taunt animation
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3db1c2c-2b90-4e60-8e17-b393d0516cc5/dbvn5to-1e78367f-627f-4b08-b25b-6ff79d8e7246.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzZGIxYzJjLTJiOTAtNGU2MC04ZTE3LWIzOTNkMDUxNmNjNVwvZGJ2bjV0by0xZTc4MzY3Zi02MjdmLTRiMDgtYjI1Yi02ZmY3OWQ4ZTcyNDYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dpZ_KpgpC3iUJ6JUO3XjRUBeSGqbeblU1xsY2LO3GT4",
+    //Link Moving animation
+    "https://64.media.tumblr.com/e35cda5ad297e1a0bbcb83257f91ccf1/tumblr_ml5gbqTWeO1rfjowdo1_500.gif",
+    // Link Taunt animation
+    "https://i.gifer.com/origin/ed/edab6122adcb8868bf28208cc9e9bf5d_w200.gif"
     ]
 
     module.exports = Sprites;
