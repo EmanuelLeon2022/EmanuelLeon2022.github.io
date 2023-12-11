0691c80a112939ca31dc48cd1138ed8e99eaa8cc
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-12-11 14:15:59 -0500

    email removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='feet'>
         <a href='tel: 0000000000'><h3>Phone</h3></a>
-        <a href='mailto:emanuell-v2022@outlook.com'><h3>Email</h3></a>
+        <a href='mailto:gmail@chucknorris.com'><h3>Email</h3></a>
         <a href='http://www.linkedin.com/in/emanuel-leon-vargas-751720296' target="_blank"><h3>LinkedIn</h3></a>
         <a href='https://github.com/EmanuelLeon2022' target="_blank"><h3>GitHub</h3></a>
     </div>
