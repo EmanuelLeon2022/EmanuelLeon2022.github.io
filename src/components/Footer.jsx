a3e319becfbca06a25cbd8ef464c94c4ecd6646e
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2024-06-11 15:41:02 -0400

    Update Footer.jsx
    Contact updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div className='feet'>
-        <a href='tel: 7023384682'><h3>Phone</h3></a>
-        <a href='mailto:gmail@chucknorris.com'><h3>Email</h3></a>
+        <a href='tel: 7029342205'><h3>Phone</h3></a>
+        <a href='mailto:emanuell-v2022@outlook.com'><h3>Email</h3></a>
         <a href='https://github.com/EmanuelLeon2022' target="_blank"><h3>GitHub</h3></a>
     </div>
   )
